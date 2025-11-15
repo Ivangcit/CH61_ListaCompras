7b963671d225fa2dee577cf46106a944002391d1
Ivangcit <gunstriptor@gmail.com>
2025-11-14 18:29:11 -0600

    Trabajando en objetivo 3

---
+++ js/main.js
@@ -0,0 +1,83 @@
+// botones
+const btnAgregar=document.getElementById('btnAgregar');
+const btnClear=document.getElementById('btnClear');
+// campos de llenadpo
+const txtName=document.getElementById('Name');
+const txtNumber=document.getElementById('Number');
+const txtPrice=document.getElementById('Price');
+//formulario
+const formularioAgregar=document.getElementById('formAgregar');
+//alerta de validaciones
+const alertValidacionesTexto=document.getElementById("alertValidacionesTexto");
+const alertValidaciones=document.getElementById("alertValidaciones");
+//
+
+
+//funciones
+function validarCantidad(cantidad){
+    if(cantidad.length==0){
+        return false;
+    }
+    if(isNaN(cantidad)){
+        return false;
+    }
+    if(Number(cantidad)<=0){
+        return false;
+    }
+    return true;
+}//validarCantidad
+
+
+function getPrecio(precio){
+    // return Math.floor(Math.random()*100);
+    return Math.round(Math.random()*10000)/100;
+}//getprecio
+
+
+//botones
+
+btnClear.addEventListener('click',function(event){
+    event.preventDefault();
+    formularioAgregar.reset();
+});
+
+btnAgregar.addEventListener('click',function(event){
+    event.preventDefault();
+    //bandera
+    let isValid=true;
+    //Para que las alertas se reinicien en cada click y marque los errores adecuadamente
+    txtName.style.border="";
+    txtNumber.style.border="";
+    txtPrice.style.border="";
+        alertValidacionesTexto.innerHTML="";
+        alertValidaciones.style.display="none";
+    
+    if(txtName.value.length<3){
+        txtName.style.border="solid thin red";
+        alertValidacionesTexto.innerHTML="<strong>El nombre del producto es incorrecto</strong><br/>";
+        alertValidaciones.style.display="block";
+        isValid=false;
+    }
+
+
+    if(!validarCantidad(txtNumber.value)){
+        txtNumber.style.border="solid thin red";
+        alertValidacionesTexto.innerHTML+="<strong>La cantidad no es correcta</strong><br/>";
+        alertValidaciones.style.display="block";
+        isValid=false;
+    }
+
+    if(!validarCantidad(txtPrice.value)){
+        txtPrice.style.border="solid thin red";
+        alertValidacionesTexto.innerHTML+="<strong>El precio no es correcto</strong>";
+        alertValidaciones.style.display="block";
+        isValid=false;
+    }    
+
+    if(isValid){
+        let precio=getPrecio();
+        console.log(precio);
+    }
+
+
+});
